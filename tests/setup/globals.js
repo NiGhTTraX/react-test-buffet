d92fb58208e03893e7554e50aa3a1c567ccaea30
Andrei Picus <office.nightcrawler@gmail.com>
2014-11-06 11:51:33 +0200

    Add a test area container for the components
    When running in your own browser, you can see how the component is rendered.

--- tests/setup/globals.js
+++ tests/setup/globals.js
@@ -14,9 +14,20 @@ chai.use(sinonChai);
 beforeEach(function() {
   /**
    * Prepare a container to mount the component before each test.
+   *
+   * If there's a container on the page named #test-area, use that, otherwise
+   * create a detached div and use that. The former case if useful when running
+   * the tests in your own browser so you can debug how the component is
+   * rendered.
    */
 
-  this.container = document.createElement('div');
+  var testArea = document.querySelector('#test-area');
+
+  if (testArea) {
+    this.container = testArea;
+  } else {
+    this.container = document.createElement('div');
+  }
 });
 
 
