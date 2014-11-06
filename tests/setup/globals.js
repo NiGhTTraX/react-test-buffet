923c464a422223ed4cf95d8f4034b9b9662389ee
Andrei Picus <office.nightcrawler@gmail.com>
2014-11-06 11:54:50 +0200

    Move the before and after hooks into a separate file

--- tests/setup/globals.js
+++ tests/setup/globals.js
@@ -1,41 +1,10 @@
 var chai = require('chai'),
     sinon = require('sinon'),
-    sinonChai = require('sinon-chai'),
-    path = require('path'),
-    React = require('react');
+    sinonChai = require('sinon-chai');
 
 
 global.expect = chai.expect;
 
 
 chai.use(sinonChai);
-
-
-beforeEach(function() {
-  /**
-   * Prepare a container to mount the component before each test.
-   *
-   * If there's a container on the page named #test-area, use that, otherwise
-   * create a detached div and use that. The former case if useful when running
-   * the tests in your own browser so you can debug how the component is
-   * rendered.
-   */
-
-  var testArea = document.querySelector('#test-area');
-
-  if (testArea) {
-    this.container = testArea;
-  } else {
-    this.container = document.createElement('div');
-  }
-});
-
-
-afterEach(function() {
-  /**
-   * Unmount the component after each test.
-   */
-
-  React.unmountComponentAtNode(this.container);
-});
 
