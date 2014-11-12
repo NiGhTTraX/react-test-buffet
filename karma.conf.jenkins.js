55bae129b8ad882d3f72e0655c396fa6837fe51f
Andrei Picus <office.nightcrawler@gmail.com>
2014-11-12 15:39:32 +0200

    Add a Karma config for Jenkins
    Extends the base conf file and sets up coverage and JUnit and Cobertura
    reporters.

---
+++ karma.conf.jenkins.js
@@ -0,0 +1,28 @@
+var filePatterns = require('./file-patterns.js');
+
+
+module.exports = function(config) {
+  var opts = require('./karma.conf.base.js')(config),
+      bundlePath = filePatterns.buildPath + filePatterns.bundleInstrumentedName;
+
+  opts.files.push(bundlePath);
+
+  var preprocessors = {};
+  preprocessors[bundlePath] = ['sourcemap'];
+  opts.preprocessors = preprocessors;
+
+  opts.browsers = ['PhantomJS'];
+
+  opts.reporters.push('junit');
+
+  opts.reporters.push('coverage');
+  opts.coverageReporter = {
+    type: 'cobertura'
+  };
+
+  opts.singleRun = true;
+
+  config.set(opts);
+};
+
+
