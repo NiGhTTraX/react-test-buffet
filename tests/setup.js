def4ba187e2e5e8a2850281baaebeea502e129d5
Andrei Picus <office.nightcrawler@gmail.com>
2015-06-14 20:22:00 +0300

    Take jsdom out of the browserify bundle

--- tests/setup.js
+++ tests/setup.js
@@ -4,11 +4,6 @@
  * Require all your setup modules here while enforcing the order between them.
  */
 
-// Check if we're in a browser. If we're not, setup jsdom.
-if (typeof document === 'undefined') {
-  require('./setup/jsdom.js');
-}
-
 require('./setup/component-mounting.js');
 
 // The sandbox should be cleared after the component has unmounted.
