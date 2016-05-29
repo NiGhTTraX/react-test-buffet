78673b734de857d99a58e03a52f68ae219b682f3
Andrei Picus <office.nightcrawler@gmail.com>
2016-05-29 18:32:02 +0300

    Add a little bit more HTML

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,10 +4,17 @@ import React, { Component } from 'react';
 export default function appFactory() {
   return class App extends Component {
     render() {
-      return <div className="todo-app">
-        <input type="text" className="new-todo" autoFocus
-          placeholder="What needs to be done?"
-        />
+      return <div className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input type="text" className="new-todo" autoFocus
+            placeholder="What needs to be done?"
+          />
+        </header>
+        <div className="main">
+        </div>
+        <footer className="footer">
+        </footer>
       </div>;
     }
   };
