53a8d1433807e946c9797b53b8a4255c818fab2f
Andrei Picus <office.nightcrawler@gmail.com>
2016-05-31 12:54:02 +0300

    Remove duplicated setup between tests

--- tests/acceptance/no-todos.spec.js
+++ tests/acceptance/no-todos.spec.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import appFactory from '../../src/components/app.jsx';
-
-
-const App = appFactory();
+import App from './app.setup.js';
 
 
 describe('App', function() {
