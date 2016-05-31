6d485f951f941e59666cc4f22aea27fcbcc4b7e7
Andrei Picus <office.nightcrawler@gmail.com>
2016-05-31 18:27:30 +0300

    Remove app setup duplication between tests and demo

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import appFactory from './components/app.jsx';
-import todoListFactory from './components/todo-list.jsx';
-
-const TodoList = todoListFactory();
-const App = appFactory(TodoList);
+import App from '../tests/acceptance/app.setup.js';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
