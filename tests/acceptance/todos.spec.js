f02e077631539b443ea8d879edb46c874408685f
Andrei Picus <office.nightcrawler@gmail.com>
2016-05-31 13:23:40 +0300

    Check that adding a new todo works

--- tests/acceptance/todos.spec.js
+++ tests/acceptance/todos.spec.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Simulate } from 'react-addons-test-utils';
 import $ from 'jquery';
 import App from './app.setup.js';
+
+
+const ENTER = 13;
 
 
 describe('App', function() {
@@ -15,9 +19,21 @@ describe('App', function() {
       $component = $(ReactDOM.findDOMNode(component));
     });
 
-    it('should focus on the todo input', function() {
+    it('should focus on the new todo input when the page loads', function() {
       expect(document.activeElement).to.equal(
         $component.find('.new-todo')[0]);
+    });
+
+    it('should add a new todo when pressing enter', function() {
+      const node = $component.find('.new-todo')[0];
+
+      // TODO: this exposes too much of the internals; figure out a way to write
+      // tests without knowing all of this
+      Simulate.change(node, { target: { value: 'buy milk' } });
+      Simulate.keyDown(node, { keyCode: ENTER });
+
+      expect($component.find('.todo')).to.have.length(1);
+      expect($component.find('.todo').text()).to.equal('buy milk');
     });
   });
 });
