3b68ca4298ca9b631716521c85f853403cdc0620
Andrei Picus <office.nightcrawler@gmail.com>
2016-06-05 14:12:33 +0300

    Mark completed todos

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,24 +1,31 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 
 export default function todoListFactory() {
-  function TodoList(props) {
-    return <ul className="todo-list">
-      {props.todos.map((todo, index) =>
-        <li className="todo" key={index}>
-          <div className="view">
-            <label>{todo.title}</label>
-          </div>
-        </li>
-      )}
-    </ul>;
-  }
+  return class TodoList extends Component {
+    static propTypes = {
+      todos: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        complete: PropTypes.bool
+      })).isRequired
+    };
 
-  TodoList.propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired
-    })).isRequired
+    render() {
+      return <ul className="todo-list">
+        {this.props.todos.map((todo, index) =>
+          <li className={classnames('todo', { completed: todo.completed })}
+            key={index}
+          >
+            <div className="view">
+              <input className="toggle" type="checkbox"
+                checked={todo.completed} readOnly
+              />
+              <label>{todo.title}</label>
+            </div>
+          </li>
+        )}
+      </ul>;
+    }
   };
-
-  return TodoList;
 }
--- tests/unit/components/todo-list/base.spec.js
+++ tests/unit/components/todo-list/base.spec.js
@@ -11,7 +11,8 @@ describe('TodoList', function() {
   const todos = [{
     title: 'buy milk'
   }, {
-    title: 'buy eggs'
+    title: 'buy eggs',
+    completed: true
   }];
 
   let $component;
@@ -23,8 +24,30 @@ describe('TodoList', function() {
   });
 
   it('should render every todo', function() {
-    $component.find('.todo').each((todo, index) => {
+    const $todos = $component.find('.todo');
+
+    expect($todos).to.have.length(2);
+
+    $todos.each((index, todo) => {
       expect($(todo).text()).to.equal(todos[index].title);
     });
+  });
+
+  it('should not mark active todos', function() {
+    const $buyMilkTodo = $component.find('.todo').eq(0);
+
+    expect($buyMilkTodo.hasClass('completed'),
+          'Todo should not be marked as completed').to.be.false;
+    expect($buyMilkTodo.find('.toggle').is(':checked'),
+          'Todo should not be checked').to.be.false;
+  });
+
+  it('should mark completed todos', function() {
+    const $buyEggsTodo = $component.find('.todo').eq(1);
+
+    expect($buyEggsTodo.hasClass('completed'),
+          'Todo should be marked as completed').to.be.true;
+    expect($buyEggsTodo.find('.toggle').is(':checked'),
+          'Todo should be checked').to.be.true;
   });
 });
