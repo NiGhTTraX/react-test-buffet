7a72f8365847d101b4177331aaaaed93bb85d142
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-01-09 18:03:36 +0200

    Add docstring for <Toggleable>

--- src/components/toggleable.jsx
+++ src/components/toggleable.jsx
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 
 
+/**
+ * A base component that can be toggled on or off.
+ *
+ * The derived class should render the actual representation of the component,
+ * including the toggle state.
+ */
 export default class Toggleable extends Component {
   static propTypes = {
     className: PropTypes.string,
