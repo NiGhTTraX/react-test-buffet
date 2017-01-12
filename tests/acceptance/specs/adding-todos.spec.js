b88d7c2b7608546aa9e308dcca976ef1dfe70bb2
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-01-12 13:32:47 +0200

    Refactor the acceptance tests for adding todos

---
+++ tests/acceptance/specs/adding-todos.spec.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+
+
+describe('App', function() {
+  describe('adding new todos', function() {
+    it('should focus on the new todo input when the page loads', function() {
+      return this.client.elementActive().getAttribute('class')
+        .then(className => { expect(className).to.contain('new-todo'); });
+    });
+
+    it('should add a new todo when pressing enter', function() {
+      return addTodo(this.client, 'buy milk')
+        .then(() => this.client.element('.todo').getText())
+        .then(text => { expect(text).to.equal('buy milk'); });
+    });
+
+    it('should trim whitespace from new todos', function() {
+      return addTodo(this.client, '   buy milk    ')
+        .then(() => this.client.element('.todo').getText())
+        .then(text => { expect(text).to.equal('buy milk'); });
+    });
+
+    it('should clear the add todo input after adding a todo', function() {
+      return addTodo(this.client, 'buy eggs')
+        .then(() => this.client.element('.new-todo').getValue())
+        .then(placeholder => { expect(placeholder).to.be.empty; });
+    });
+
+    it('should not add an empty todo', function() {
+      return addTodo(this.client, '')
+        .then(() => this.client.elements('.todo'))
+        .then(({ value: todos }) => { expect(todos).to.be.empty; });
+    });
+  });
+});
+
+function addTodo(client, todo) {
+  return client.click('.new-todo')
+    .then(() => client.keys(todo))
+    .then(() => client.keys(['Enter']));
+}
