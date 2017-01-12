c77f7d102143f30398472362d1c3e57dcba1b518
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-01-12 14:50:11 +0200

    Document how to connect with VNC to the Selenium server in debug mode

--- tests/acceptance/debug.sh
+++ tests/acceptance/debug.sh
@@ -4,6 +4,8 @@ cd tests/acceptance
 
 docker-compose build
 
+# After the selenium container is up connect to vnc://localhost:5900 with
+# password 'secret'.
 docker-compose -f docker-compose.yml -f docker-compose.debug.yml \
   run --service-ports tests
 RESULT=$?
