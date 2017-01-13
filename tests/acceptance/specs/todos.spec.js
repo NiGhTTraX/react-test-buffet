6a58806d7db53a3acd3d2d6c874e680f583e8322
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-01-13 14:33:24 +0200

    Reduce duplication in acceptance tests

--- tests/acceptance/specs/todos.spec.js
+++ tests/acceptance/specs/todos.spec.js
@@ -11,23 +11,24 @@ describe('App', function() {
 
   describe('todos', function() {
     it('should not be marked as completed after being added', function() {
-      // We can use getAttribute without map after
-      // https://github.com/webdriverio/wdio-sync/issues/43 is fixed.
-      const states = $$('.todo .toggle').map(c => c.getAttribute('checked'));
-
-      expect(states).to.have.length(3);
-
-      expect(states.every(state => state === 'true')).to.be.false;
+      expect(allTodosChecked()).to.be.false;
     });
 
     it('should be marked as completed when checking them', function() {
       $$('.todo .toggle').forEach(toggle => toggle.click());
 
-      const states = browser.elements('input[type=checkbox]').getAttribute('checked');
+      expect(allTodosChecked()).to.be.true;
+    });
+
+    function allTodosChecked() {
+      // We can use getAttribute without map after
+      // https://github.com/webdriverio/wdio-sync/issues/43 is fixed.
+      const states = $$('.todo .toggle').map(
+        toggle => toggle.getAttribute('checked'));
 
       expect(states).to.have.length(3);
 
-      expect(states.every(state => state === 'true')).to.be.true;
-    });
+      return states.every(state => state === 'true');
+    }
   });
 });
