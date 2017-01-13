ef622519f82653d6e7e754e6483adc125d7fb654
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-01-13 14:34:44 +0200

    Don't timeout Mocha when debugging the tests

--- tests/acceptance/wdio.conf.debug.js
+++ tests/acceptance/wdio.conf.debug.js
@@ -2,5 +2,9 @@ const { config } = require('./wdio.conf.js');
 
 exports.config = Object.assign({}, config, {
   // Start only a single browser for easy monitoring via VNC.
-  maxInstances: 1
+  maxInstances: 1,
+
+  mochaOpts: Object.assign({}, config.mochaOpts, {
+    timeout: 24 * 60 * 60 * 1000
+  })
 });
