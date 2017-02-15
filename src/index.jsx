d90970c35357fed80eb66649edf19cc8621325c9
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-02-15 23:03:05 +0100

    Fix hot reloading
    Upgrading to webpack@2 broke it. Switching to react-hot-loader@3 is its own can
    of worms:
    
    - the root component needs to be wrapped in a provider,
    - the hot module replacement API needs to be used explicitly in the entry point,
    - the next version of the root component needs to be `require`-ed on hot
      updates,
    - react-hot-loader is currently in beta.
    
    Figure out if this is worth it. The alternative is to downgrade back to
    webpack@1.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './app-factory.js';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppContainer><App /></AppContainer>,
+                document.getElementById('root'));
+
+// TODO: maybe move this to an index.dev.js?
+if (module.hot) {
+  module.hot.accept('./app-factory.js', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./app-factory.js').default;
+
+    ReactDOM.render(<AppContainer><NextApp /></AppContainer>,
+                    document.getElementById('root'));
+  });
+}
