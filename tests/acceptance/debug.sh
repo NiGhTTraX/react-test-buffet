9a4b1d7606572a41749cc5cc8cfcd375d4e69686
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-02-26 10:13:36 +0100

    Simplify debug script
    We don't care about exit codes when debugging.

--- tests/acceptance/debug.sh
+++ tests/acceptance/debug.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
+set -e
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 docker-compose build
-RESULT=$?
-
-if [[ $RESULT != 0 ]]; then
-  exit $RESULT
-fi
-
 docker-compose -f docker-compose.yml -f docker-compose.debug.yml \
   run --service-ports tests
-RESULT=$?
-
 docker-compose down
-
-exit $RESULT
