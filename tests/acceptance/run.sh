770d1886230443139ad531d10e518c1beab9fdce
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-02-26 11:51:55 +0100

    Fix screenshots folder being created with the wrong permissions

--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -9,6 +9,9 @@ if [[ $RESULT != 0 ]]; then
   exit $RESULT
 fi
 
+# If we don't create it here, docker-compose will and it will be owned by root.
+mkdir -p screenshots
+
 docker-compose run tests
 RESULT=$?
 
