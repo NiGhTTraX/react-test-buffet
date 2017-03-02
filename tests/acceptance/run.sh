6d1f335dea199224c3968ac0b1dc8a6433e15760
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-03-02 18:12:22 +0100

    Use Selenium Hub instead of standalone Selenium
    This will allow us to run the acceptance tests on more than one type of browser,
    or have them running in parallel on multiple instances.

--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -12,6 +12,11 @@ fi
 # If we don't create it here, docker-compose will and it will be owned by root.
 mkdir -p screenshots
 
+docker-compose up -d selenium
+echo Waiting for Chrome to connect to the Selenium hub...
+# TODO: poll the hub for its status
+sleep 5
+
 docker-compose run tests
 RESULT=$?
 
