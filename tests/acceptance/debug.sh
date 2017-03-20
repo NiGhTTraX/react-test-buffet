4f60d9b20b3626cdb4e4634875a58f9b7cdd2347
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-03-20 20:14:52 +0100

    Fix stale content in acceptance debug container

--- tests/acceptance/debug.sh
+++ tests/acceptance/debug.sh
@@ -4,6 +4,6 @@ set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-docker-compose build
+docker-compose -f docker-compose.debug.yml build
 docker-compose -f docker-compose.debug.yml run --service-ports acceptance_debug
 docker-compose down
