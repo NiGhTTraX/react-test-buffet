09f4d1c41b8054be2b9049f75196481f6025a4ea
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-04-02 18:43:24 +0200

    Add some more comments in the acceptance running script

--- tests/acceptance/run.sh
+++ tests/acceptance/run.sh
@@ -18,9 +18,10 @@ echo Waiting for the browsers to connect to the Selenium hub...
 # TODO: poll the hub for its status
 sleep 5
 
-# compose exits with 0 no matter what.
+# compose up exits with 0 no matter what.
 docker-compose up acceptance_chrome acceptance_firefox
 
+# Aggregate results from all the containers.
 RESULT=$(docker-compose ps -q \
   | xargs docker inspect -f '{{ .State.ExitCode }}' \
   | grep -v 0 | wc -l | tr -d ' ')
