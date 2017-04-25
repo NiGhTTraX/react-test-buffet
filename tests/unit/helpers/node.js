7cd70265996f19b48331aa85219c496882be3771
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-04-25 08:16:28 +0200

    Use the new API for jsdom

--- tests/unit/helpers/node.js
+++ tests/unit/helpers/node.js
@@ -1,12 +1,12 @@
 /**
  * @fileoverview Setup a virtual DOM for testing in NodeJS.
  */
-import { jsdom } from 'jsdom';
+import { JSDOM } from 'jsdom';
 
 
 // React needs these.
-global.document = jsdom('');
-global.window = document.defaultView;
+global.window = new JSDOM('').window;
+global.document = global.window.document;
 global.navigator = {
   userAgent: 'node.js'
 };
