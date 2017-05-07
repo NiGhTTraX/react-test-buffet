c30ac266401a037e0d008d3ae1aa1bdb58a94c66
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-05-07 14:40:16 +0200

    Move fixtures to playground folder

--- cosmos.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  componentPaths: ['src/components'],
-  webpackConfigPath: 'src/webpack.config.dev.js',
-  fixturePaths: ['tests/fixtures'],
-  publicPath: 'playground',
-  hmrPlugin: false
-};
---
+++ playground/cosmos.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  componentPaths: ['../src/components'],
+  webpackConfigPath: '../src/webpack.config.dev.js',
+  fixturePaths: ['./fixtures'],
+  publicPath: './',
+  hmrPlugin: false
+};
