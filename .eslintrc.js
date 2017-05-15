ed5d15848a6f6b5367306e254b098997c940e274
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-05-15 22:12:53 +0200

    Allow autofocused elements

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,6 +66,13 @@ module.exports = {
         'everything-else',
         '/^_.+$/' // private methods
       ]
-    }]
+    }],
+
+    // Autofocus is said to dissorient users with cognitive disabilities.
+    // However, the same spec
+    // (https://w3c.github.io/html/sec-forms.html#autofocusing-a-form-control-the-autofocus-attribute)
+    // says that user agents should provide a way to disable autofocus
+    // behaviour.
+    'jsx-a11y/no-autofocus': 0
   }
 }
