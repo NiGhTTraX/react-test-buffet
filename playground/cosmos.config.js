4b53fe376cabdf62528afdedd4c80ea8caf61bba
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-05-26 17:42:01 +0200

    Add TODO for removing global imports from playground
    Because they break encapsulation.

--- playground/cosmos.config.js
+++ playground/cosmos.config.js
@@ -2,6 +2,8 @@ module.exports = {
   componentPaths: ['../src/components'],
   webpackConfigPath: '../src/webpack.config.dev.js',
   fixturePaths: ['./fixtures'],
+  // TODO: remove this and have every component depend on it
+  // https://github.com/react-cosmos/react-cosmos/pull/364
   globalImports: ['../src/index.less'],
   publicPath: './',
   hmrPlugin: false
