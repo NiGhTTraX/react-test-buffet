70b2a0eeebe1403abaaa8b1c7cfec2899aa98308
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-08-14 07:51:33 +0200

    Remove TODO about npm dep in the lint hook

--- tools/git-hooks/lint.sh
+++ tools/git-hooks/lint.sh
@@ -15,9 +15,10 @@ if [ $? != 0 ]; then
 fi
 
 
+# This requires npm to be installed on the host. While this project aims to
+# provide a Docker image to run the app in dev mode, some dev dependencies are
+# best installed on the host.
 # The --silent flag is to not output the entire npm ERR epilogue.
-# TODO: this requires npm to be installed on the host; either document or work
-# around it (maybe run it through docker?)
 npm run --silent lint:files -- $CHANGED_JS_FILES
 
 
