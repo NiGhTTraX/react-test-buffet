ea69e4a2dd460f60202cb6b2501c4550cbf2b6ce
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-09-03 15:56:06 +0200

    Import common styles in app entry point

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app-factory.js';
+import './index.less';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
