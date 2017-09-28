7793e5a6abda67d86823f4055aed23205b18fd8d
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-09-28 23:24:40 +0200

    Remove TODO about global import in playground
    It's OK to have an index.less file with body styles.

--- playground/cosmos.config.js
+++ playground/cosmos.config.js
@@ -5,7 +5,6 @@ module.exports = {
   ],
   webpackConfigPath: '../src/webpack.config.dev.js',
   fixturePaths: ['./fixtures'],
-  // TODO: remove this and have every component depend on it
-  // https://github.com/react-cosmos/react-cosmos/pull/364
+
   globalImports: ['../src/index.less']
 };
