f2893d72e4ee7cac0c2eb53adc7805acfc98f59e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-03-16 16:36:12 +0100

    Don't restart Selenium when TDD-ing

--- tests/acceptance/selenium.sh
+++ tests/acceptance/selenium.sh
@@ -13,6 +13,18 @@ mkdir -p screenshots/chrome screenshots/firefox
 rm -rf ./acceptance/results
 mkdir -p ./acceptance/results/coverage
 
+# Check if the hub is already ready form a previous run.
+# When running the acceptance tests the hub container will
+# always get removed so there's no risk of staleness. This
+# is an optimization for TDD-ing.
+set +e
+./wait-for-nodes.sh 2 0 2>&1 > /dev/null
+if [[ $? == 0 ]]; then
+    echo Hub was already ready.
+    exit 0
+fi
+set -e
+
 docker-compose build
 docker-compose up -d selenium
 
