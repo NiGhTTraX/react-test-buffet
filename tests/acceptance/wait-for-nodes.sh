9861dbd1af4db79468c5ac4e4bf235dd22a43cf4
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-03-16 16:20:41 +0100

    Wait a max amount for Selenium to be ready

--- tests/acceptance/wait-for-nodes.sh
+++ tests/acceptance/wait-for-nodes.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# How many browsers are we expecting to connect to the hub.
 EXPECTED_BROWSERS=$1
-HOST=${2:-localhost}
-PORT=${3:-4444}
+
+# How many seconds should we wait for the browsers to connect?
+WAIT=${2:-10}
+
+# Selenium hub hostname.
+HOST=${3:-localhost}
+
+# Selenium hub port number.
+PORT=${4:-4444}
 
 
 nodes_connected() {
@@ -14,16 +22,24 @@ nodes_connected() {
     return
   fi
 
-  echo $status | python -c \
+  echo ${status} | python -c \
     'import json,sys;obj=json.load(sys.stdin);print obj["slotCounts"]["free"]'
 }
 
 echo Waiting for ${EXPECTED_BROWSERS} browsers to connect to the Selenium hub...
 
+PINGS=0
+
 while true; do
-  if [[ $(nodes_connected $HOST $PORT) == $EXPECTED_BROWSERS ]]; then
+  if [[ $(nodes_connected ${HOST} ${PORT}) == ${EXPECTED_BROWSERS} ]]; then
     printf "\n"
     exit 0
+  fi
+
+  PINGS=$((PINGS+1))
+
+  if [ ${PINGS} -ge ${WAIT} ] ; then
+    exit 1
   fi
 
   printf "."
