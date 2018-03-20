586fa3dc5f579bf436aed287a0daab650b61c13f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-03-20 22:54:19 +0100

    Remove the Webpack build for unit tests

--- tests/unit/helpers/index.js
+++ tests/unit/helpers/index.js
@@ -4,4 +4,5 @@
  * The modules imported here may have side effects and the import order is
  * important.
  */
+import './node.js';
 import './react-warnings.js';
--- tests/unit/helpers/index.node.js
+++
@@ -1,9 +0,0 @@
-/*
- * @fileoverview Test helpers for NodeJS..
- *
- * The modules imported here may have side effects and the import order is
- * important.
- */
-import './node.js';
-
-import './index.js';
