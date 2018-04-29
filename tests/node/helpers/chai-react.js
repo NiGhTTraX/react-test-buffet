3a9c87f7874abb8e7119cb93597527ad5b0807c2
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-04-29 13:58:49 +0200

    Remove unused createSpy

--- tests/node/helpers/chai-react.js
+++ tests/node/helpers/chai-react.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { match, spy, stub } from 'sinon';
+import { match, stub } from 'sinon';
 import { inspect } from 'util';
 
 
@@ -116,80 +115,4 @@ export function createReactStub() {
   });
 
   return reactStub;
-}
-
-/**
- * @param {Object} options
- * @param {String} options.name The display name of the component.
- * @param {ReactClass} [options.Mock] If given, the spy will try to look like
- *     it. Currently it only copies the prop types.
- *     TODO: copy public methods; maybe inherit?
- *
- * @returns {ReactClass}
- */
-export function createSpy({ name = 'Spy', Mock = Component } = {}) {
-  const _renderSpy = spy();
-
-  return class Spy extends Component {
-    static displayName = name;
-
-    static propTypes = Mock.propTypes;
-
-    /**
-     * Get whether the component was rendered at least once.
-     *
-     * @return {Boolean}
-     */
-    static get rendered() {
-      return _renderSpy.called;
-    }
-
-    /**
-     * Get the props used in the last render.
-     *
-     * @return {Object}
-     */
-    static get lastProps() {
-      if (!_renderSpy.called) {
-        throw new Error('Component was never rendered');
-      }
-
-      return _renderSpy.args[_renderSpy.callCount - 1][0];
-    }
-
-    /**
-     * Get the props used in every render ever.
-     *
-     * @returns {Object[]}
-     */
-    static get renders() {
-      if (!_renderSpy.called) {
-        throw new Error('Component was never rendered');
-      }
-
-      return _renderSpy.args.map(args => args[0]);
-    }
-
-    /**
-     * Check if the component was rendered at least once with the given props.
-     *
-     * @param {Object} props Supports sinon matchers. See
-     * http://sinonjs.org/docs/#sinon-match-api.
-     *
-     * @returns {Boolean}
-     */
-    static renderedWith(props) {
-      return _renderSpy.calledWithMatch(props);
-    }
-
-    static reset() {
-      _renderSpy.resetHistory();
-    }
-
-    render() {
-      _renderSpy(this.props);
-
-      return <div>I am a fake component, here to spy on you!</div>;
-    }
-  };
 }
