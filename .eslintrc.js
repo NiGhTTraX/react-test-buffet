b8a94dfa46eaccabf06dc33ba8ef28d1ed916d64
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-24 17:14:49 +0100

    Upgrade to the latest ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   'root': true,
 
-  'parser': 'babel-eslint',
-
   'extends': [
-    '@nighttrax/eslint-config/jsx',
-    '@nighttrax/eslint-config/react-a11y',
-    '@nighttrax/eslint-config/imports'
-  ].map(require.resolve)
+    '@nighttrax/eslint-config-jsx'
+  ]
 };
